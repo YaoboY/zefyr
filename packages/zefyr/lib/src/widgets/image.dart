dabec28f92a5d51d4a3e7a994f6ceb1edac8f873
YaoboY <50326774+YaoboY@users.noreply.github.com>
2020-03-24 18:00:20 +0800

    Update image.dart

--- packages/zefyr/lib/src/widgets/image.dart
+++ packages/zefyr/lib/src/widgets/image.dart
@@ -220,7 +220,8 @@ class RenderEditableImage extends RenderBox
         minWidth: 0.0,
         maxWidth: width,
         minHeight: 0.0,
-        maxHeight:(width * 16 / 9).floorToDouble(),
+//          maxHeight:(width * 16 / 9).floorToDouble(),
+        maxHeight:constraints.maxHeight,
       );
       child.layout(childConstraints, parentUsesSize: true);
       _lastChildSize = child.size;
